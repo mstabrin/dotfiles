b39c720f83a70d54fca0a08505c313510c1b2dd4
Markus Stabrin <Markus.Stabrin@mpi-dortmund.mpg.de>
2017-04-18 15:12:41 +0200

    Removed echo from reset envs

--- reset_env.zsh
+++ reset_env.zsh
@@ -5,15 +5,12 @@ DEFAULT_ALIAS=${ZSHRC_DEFAULT_ALIAS}
 # Reset env
 for entry in `env | sed 's/=.*//'`
 do
-    echo $entry
     (unset ${entry} 2>/dev/null) || continue
-    echo $entry
     unset ${entry}
 done
 
 # Reset aliases
 unalias -m '*'
-alias
 
 # Load default env and alias
 source ${DEFAULT_ENV}

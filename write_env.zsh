a8fe88fd416a3cf360c4d8c58503ef2da5c4daf9
Markus Stabrin <Markus.Stabrin@mpi-dortmund.mpg.de>
2017-04-18 15:08:53 +0200

    Fixed reset of environmental variables

--- write_env.zsh
+++ write_env.zsh
@@ -1,3 +1,3 @@
 # Write files
-env | sed 's/\(^.*\)/export \1/g' > ${ZSHRC_DEFAULT_ENV}
+printenv | grep -v _=| grep -v module= | grep -v '}' | sed 's/^\([A-Za-z0-9_]*=\)\(.*\)$/export \1"\2"/g' > ${ZSHRC_DEFAULT_ENV}
 alias | sed 's/\(^.*\)/alias \1/g' > ${ZSHRC_DEFAULT_ALIAS}

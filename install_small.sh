05f591c9a3a170cfabe157aaac40b5a6fdf3c079
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2021-11-08 12:16:17 +0100

    Exchange c++ coc

--- install_small.sh
+++ install_small.sh
@@ -20,7 +20,7 @@ git submodule update
 git submodule update
 
 vim -u ${VIMRC_FILE} +PluginInstall +"exec coc#util#install()" +qall
-vim -u ${VIMRC_FILE} +"CocInstall coc-pyright coc-json coc-tsserver coc-yaml coc-sh coc-ccls" +qall
+vim -u ${VIMRC_FILE} +"CocInstall coc-pyright coc-json coc-tsserver coc-yaml coc-sh coc-clangd" +qall
 
 mv ${VIM_SOURCE_FILE} ${VIM_SOURCE_BACKUP_FILE}
 for file in /usr/bin/vim*

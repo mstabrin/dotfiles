0cda0668d30be19c9707e3efa17dde5f54e2a005
Markus Stabrin <stabrin@m21003-lin.client.mpi-dortmund.mpg.de>
2021-11-09 13:09:34 +0100

    Remove vim_small file

--- vim_small.sh
+++
@@ -1,3 +0,0 @@
-alias vim="/usr/bin/vim -u /Users/stabrin/Desktop/git/dotfiles/vimrc"
-alias vimdiff="/usr/bin/vimdiff -u /Users/stabrin/Desktop/git/dotfiles/vimrc"
-alias vimtutor="/usr/bin/vimtutor -u /Users/stabrin/Desktop/git/dotfiles/vimrc"
